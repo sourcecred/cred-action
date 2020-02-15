acae0ba5fee6d5bfd0fbe6bf1357d18968ac2be5
William Chargin <wchargin@gmail.com>
2020-02-15 08:23:26 -0700

    Simplify commands with alternate value expansion
    We introduced Bash arrays here as the most direct fix for the broken
    parameter expansion, but it’s simpler to just use alternate value
    expansion, which isn’t a Bash-ism.
    
    wchargin-branch: array-to-altvalue

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,19 +10,11 @@ set -o pipefail
 echo "Found files in workspace:"
 ls
 
-build_cmd=( /bin/bash /build_static_site.sh  --target "${INPUT_TARGET}" )
-
 # Project file and if are both required for generation!
 # The action requires, but we have the double check here for a fallback
 if [ -z "${INPUT_PROJECT_FILE}" ]; then
     echo "Project file (project-file) is required"
     exit 1
-fi
-build_cmd+=( --project-file "${INPUT_PROJECT_FILE}" )
-
-# Weights are not required, added if defined
-if [ ! -z "${INPUT_WEIGHTS}" ]; then
-    build_cmd+=( --weights "${INPUT_WEIGHTS}" )
 fi
 
 # Show the user where we are
@@ -33,9 +25,14 @@ ls
 # Clean up any previous runs (deployed in docs folder)
 # This command needs to be run relative to sourcecred respository
 # that is located at the WORKDIR /code
-rm -rf "${INPUT_TARGET}"
-printf '%s\n' "${build_cmd[*]}"
-"${build_cmd[@]}"
+(
+    set -x;
+    /bin/bash /build_static_site.sh \
+        --target "${INPUT_TARGET}" \
+        --project-file "${INPUT_PROJECT_FILE}" \
+        ${INPUT_WEIGHTS:+--weights "${INPUT_WEIGHTS}"} \
+        ;
+)
 
 echo "Finished initial run, present working directory is ${PWD}"
 ls
@@ -43,13 +40,13 @@ ls
 # This interacts with node sourcecred.js
 # Load it twice so we can access the scores -- it's a hack, pending real instance system
 # Note from @vsoch: these variable names aren't consistent - the project here referes to the project file.
-load_cmd=( node /code/bin/sourcecred.js load \
-    --project "${INPUT_PROJECT_FILE}" )
-if [ ! -z "${INPUT_WEIGHTS}" ]; then
-    load_cmd+=( --weights "${INPUT_WEIGHTS}" )
-fi
-printf '%s\n' "${load_cmd[*]}"
-"${load_cmd[@]}"
+(
+    set -x;
+    node /code/bin/sourcecred.js load \
+        --project "${INPUT_PROJECT_FILE}" \
+        ${INPUT_WEIGHTS:+--weights "${INPUT_WEIGHTS}"} \
+        ;
+)
 node /code/bin/sourcecred.js scores "${INPUT_PROJECT}" | python3 -m json.tool > "${INPUT_SCORES_JSON}"
 
 # Automated means that we push to a branch, otherwise we open a pull request
